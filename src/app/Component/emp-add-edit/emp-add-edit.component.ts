8a3a31d7016d38c181178d026e4b7f1184c7f13c
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-08-31 18:37:52 +0530

    create close popup

--- src/app/Component/emp-add-edit/emp-add-edit.component.ts
+++ src/app/Component/emp-add-edit/emp-add-edit.component.ts
@@ -3,49 +3,49 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { EmployeeService } from 'src/app/services/employee.service';
 
-
 @Component({
   selector: 'app-emp-add-edit',
   templateUrl: './emp-add-edit.component.html',
-  styleUrls: ['./emp-add-edit.component.scss']
+  styleUrls: ['./emp-add-edit.component.scss'],
 })
 export class EmpAddEditComponent {
   empForm: FormGroup;
 
-  education: string[] = [
-    'Matric',
-    'Diploma',
-    'Intermediate',
-    'Post Graduate'
-  ];
+  education: string[] = ['Matrix', 'Diploma', 'Intermediate', 'Post Graduate'];
 
-  constructor(private _formBuilder:FormBuilder,
-    private empService:EmployeeService,
-    private _dialogRef:DialogRef<EmpAddEditComponent>){
-      this.empForm = _formBuilder.group({
-        firstName: '',
-        lastName: '',
-        email: '',
-        dateOfBirth: '',
-        gender: '',
-        education: '',
-        companyName: '',
-        experience: '',
-        package: ''
-      });
+  constructor(
+    private _formBuilder: FormBuilder,
+    private empService: EmployeeService,
+    private _dialogRef: DialogRef<EmpAddEditComponent>
+  ) {
+    this.empForm = _formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dateOfBirth: '',
+      gender: '',
+      education: '',
+      companyName: '',
+      experience: '',
+      package: '',
+    });
   }
 
-  onFormSubmit(){
+  onFormSubmit() {
     if (this.empForm.valid) {
       this.empService.addEmployee(this.empForm.value).subscribe({
         next: (val: any) => {
           alert('Employee added successfully.');
           this._dialogRef.close();
         },
-        error: (err:any) => {
+        error: (err: any) => {
           console.log(err);
-        }
-      })
+        },
+      });
     }
+  }
+
+  closePopup() {
+    this._dialogRef.close();
   }
 }
